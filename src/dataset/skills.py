fbd94dbcf1e409cd6aa8ce4f850271374f78743b
tra371 <thuraaung.variable@gmail.com>
2024-10-17 02:03:03 +0700

    commit scripts

---
+++ src/dataset/skills.py
@@ -0,0 +1,293 @@
+primary_skills = [
+    "Business",
+    "Specialty",
+    "Law",
+    "Health",
+    "Education",
+    "Information Technology",
+    "Engineering"
+]
+
+business_skills = {
+    "Finance_and_Accounting": [
+        "https://osmt.wgu.edu/api/collections/1db79be3-ff33-4114-8449-e151aa9e6b25",
+        "https://osmt.wgu.edu/api/collections/701709c6-cdcb-4591-9a69-1a99db829eee",
+        "https://osmt.wgu.edu/api/collections/311eec7b-25ed-4645-a466-ab05762014c0",
+        "https://osmt.wgu.edu/api/collections/2c253149-0aee-4d59-b280-3d9b8cebe899",
+        "https://osmt.wgu.edu/api/collections/5891c84d-a84a-42b4-8474-88eca05649f4",
+        "https://osmt.wgu.edu/api/collections/8a8b0a21-8f44-4499-a7a5-05224f05c4b1",
+        "https://osmt.wgu.edu/api/collections/5e22819e-5264-4122-b118-024b8922d637",
+        "https://osmt.wgu.edu/api/collections/48e1416b-2b83-4c14-8f27-5abe148bd4cb",
+        "https://osmt.wgu.edu/api/collections/c3e0ab85-fea5-42ef-9461-cbcb9e2bc838",
+        "https://osmt.wgu.edu/api/collections/e4e89fcd-e7ab-493e-9865-a0b9b071f342"
+    ],
+    "Management": [
+        "https://osmt.wgu.edu/api/collections/b24d9ed8-c2bf-4005-a753-a5cf51ddc091",
+        "https://osmt.wgu.edu/api/collections/8248933b-2a75-46d7-a69b-20d3c7a0dbc6",
+        "https://osmt.wgu.edu/api/collections/92e554be-7518-4e2a-ae84-8f41d9f130b3",
+        "https://osmt.wgu.edu/api/collections/933d6fb0-4941-48d7-af95-7cbe2bea7db0",
+        "https://osmt.wgu.edu/api/collections/a039d109-d2d7-444f-98a3-8f428a3a3b2a",
+        "https://osmt.wgu.edu/api/collections/eae54b5f-1fb6-4302-b419-14bca0d4e905",
+        "https://osmt.wgu.edu/api/collections/ad4ce6c5-6cc1-4d8c-94d1-9654c3b93e1b",
+        "https://osmt.wgu.edu/api/collections/58091184-a788-40f3-8709-39a8a0f22213",
+        "https://osmt.wgu.edu/api/collections/f21ac707-c599-4600-8b46-f30babba2420",
+        "https://osmt.wgu.edu/api/collections/3db5cb7b-6e03-4d96-8e95-83d15d1525a8",
+        "https://osmt.wgu.edu/api/collections/3104cb5a-4f6b-47a6-97c0-5d046143f60f"
+    ],
+    "Marketing": [
+        "https://osmt.wgu.edu/api/collections/0d5ccab2-14e7-41ed-9588-601423cd7d18",
+        "https://osmt.wgu.edu/api/collections/2815440a-6fd8-4f7d-9917-51639e96dfad",
+        "https://osmt.wgu.edu/api/collections/8ed5bcb5-89e1-4eb1-aa09-5ada169b6706",
+        "https://osmt.wgu.edu/api/collections/452391bb-056d-4a5e-83ee-850440a208c0",
+        "https://osmt.wgu.edu/api/collections/a2307c58-b890-48b7-8ae2-0bd22a41ccc4",
+        "https://osmt.wgu.edu/api/collections/3eb92ca5-ffe2-42e0-b029-2809609d4418",
+        "https://osmt.wgu.edu/api/collections/9c7c5db4-ef5f-4e1d-9363-b67399459d8b"
+    ],
+    "Business Administration": [
+        "https://osmt.wgu.edu/api/collections/5e11d83b-dacf-4c94-9180-7ff67bd7ef43",
+        "https://osmt.wgu.edu/api/collections/8cd25a25-5388-42b0-8e47-e7fc0c1a1a32"
+    ],
+    "Specialty": [
+        "https://osmt.wgu.edu/api/collections/41f3cbb4-1e70-4766-84e2-a9855c6645de",
+        "https://osmt.wgu.edu/api/collections/4f8095e2-afb0-48cf-bb30-d1d135899355"
+    ],
+    "Operations": [
+        "https://osmt.wgu.edu/api/collections/9cfde8b6-f8c8-471a-a66c-9aabe554fc01",
+        "https://osmt.wgu.edu/api/collections/a095ec4a-68d7-4294-8f48-e6c2c4d49e90",
+        "https://osmt.wgu.edu/api/collections/917f22d8-0fc2-4c25-8e0e-e56522466d75",
+        "https://osmt.wgu.edu/api/collections/96e2e0db-5172-48d5-a630-0957b41721df",
+        "https://osmt.wgu.edu/api/collections/410b0e4b-f6df-43bc-8d3a-57c66f89dabc"
+    ],
+    "Human Resources": [
+        "https://osmt.wgu.edu/api/collections/f64ef6f9-c345-46de-89e3-fd7e1f3bff48",
+        "https://osmt.wgu.edu/api/collections/e162c5ae-c21b-40ff-96c4-6ca83e236333",
+        "https://osmt.wgu.edu/api/collections/0eb4acd2-e5a3-45b1-a83d-e9f970e67856",
+        "https://osmt.wgu.edu/api/collections/8513f901-da33-4cc7-ac0a-d80595f231d8",
+        "https://osmt.wgu.edu/api/collections/fa7661c0-ae50-4afb-b73f-bf1c2a97eb9a",
+        "https://osmt.wgu.edu/api/collections/b5b99d44-9feb-4588-8f35-291aa148101c",
+        "https://osmt.wgu.edu/api/collections/21daebaa-3e1b-4603-b53e-8e8344e8d8d0",
+        "https://osmt.wgu.edu/api/collections/6faf45a9-7b18-48bf-bb9f-4dd43e2d0e8a"
+    ],
+    "Product and Experience Design": [
+        "https://osmt.wgu.edu/api/collections/f5910830-a387-446a-9b00-86eb141d1d36"
+    ],
+    "Retail": [
+        "https://osmt.wgu.edu/api/collections/427374f7-cd23-4eb8-99e5-c7c1ed6e21c4",
+        "https://osmt.wgu.edu/api/collections/61c1ba5e-fb70-4d1e-9ad7-0c0f1bdd18c0"
+    ],
+    "Communications": [
+        "https://osmt.wgu.edu/api/collections/85898eab-70d1-4413-93ce-8c54f57602e0"
+    ]
+}
+
+specialty_skills = {
+    "AI": [
+        "https://osmt.wgu.edu/api/collections/7bfe0550-a69e-42a7-b7f1-b5c23517a516",
+        "https://osmt.wgu.edu/api/collections/6e219577-8a40-4c95-b52a-c3dddc1a4005",
+        "https://osmt.wgu.edu/api/collections/45236530-a823-4447-8bff-9f979a17b2b0",
+        "https://osmt.wgu.edu/api/collections/fe4353af-f802-4e83-acd2-7fc217b187c4",
+        "https://osmt.wgu.edu/api/collections/e4e23fba-1124-4f62-a6af-ac241aa13cc6",
+        "https://osmt.wgu.edu/api/collections/4944514e-b513-41c4-8597-50e86b3fce63"
+    ],
+    "Career Success": [
+        "https://osmt.wgu.edu/api/collections/80be6819-cf5d-44c7-ad5e-3645c7bf8bf0",
+        "https://osmt.wgu.edu/api/collections/3321beea-febd-4139-b3f4-7f14c4707618",
+        "https://osmt.wgu.edu/api/collections/b6fe4eea-e837-490e-8d56-592e885f27be"
+    ],
+    "Career Readiness": [
+        "https://osmt.wgu.edu/api/collections/b37587df-1911-4ac3-9f6b-3d5bfd355811",
+        "https://osmt.wgu.edu/api/collections/a3669605-3770-48f6-b60f-e3919010c2fc"
+    ],
+    "Caregiver": [
+        "https://osmt.wgu.edu/api/collections/920031d9-53ae-4d41-8140-683dc64fa84a"
+    ],
+    "Diversity, Equity, and Inclusion": [
+        "https://osmt.wgu.edu/api/collections/c74deedd-3a46-4e82-b45f-eca6aca3a87d",
+        "https://osmt.wgu.edu/api/collections/3ac93561-352e-4d22-b15c-170e09ff2a2d"
+    ],
+    "Student Success": [
+        "https://osmt.wgu.edu/api/collections/e22cef2f-5f03-4521-aa04-cd52e97c05c1"
+    ],
+    "Research": [
+        "https://osmt.wgu.edu/api/collections/3ef2592e-922d-4758-ba8d-4bded96e513a"
+    ],
+    "Skills Architecture": [
+        "https://osmt.wgu.edu/api/collections/d1e0fd24-55e0-45c0-8200-0c3b3e97862d",
+        "https://osmt.wgu.edu/api/collections/1f709241-9b2b-4369-b045-bbf75e1b8a85",
+        "https://osmt.wgu.edu/api/collections/923f34ed-954e-4c9a-8804-7cbdc31c991b",
+        "https://osmt.wgu.edu/api/collections/1ecf7918-b547-440f-8a86-6b7bc37f379e"
+    ],
+    "Social Emotional Learning": [
+        "https://osmt.wgu.edu/api/collections/c3ebefcb-6e28-49e6-b965-eb8513298384",
+        "https://osmt.wgu.edu/api/collections/9be00dcc-d812-42e0-9662-aa5336cf970c",
+        "https://osmt.wgu.edu/api/collections/3b9075ec-a1de-4a80-bbbf-e1102a5dd220",
+        "https://osmt.wgu.edu/api/collections/8f4bb4e1-46a9-4fd5-9714-aa5f0fcbc0fa",
+        "https://osmt.wgu.edu/api/collections/0e594bb4-9248-4904-b375-73eb3a32e462",
+        "https://osmt.wgu.edu/api/collections/9fd61243-96f0-4319-a56d-e638503e4777",
+        "https://osmt.wgu.edu/api/collections/b3af69da-3480-49c5-92d7-d946497924ae"
+    ]
+}
+
+law_skills = {
+    "Protective Services": [
+        "https://osmt.wgu.edu/api/collections/8959bdc8-7d46-41e2-91cf-e5abfd2ad09f",
+        "https://osmt.wgu.edu/api/collections/4359746a-a73c-4266-9f90-1c52b41350a3"
+    ],
+    "Law Enforcement": [
+        "https://osmt.wgu.edu/api/collections/873d7ef2-d1d1-4a54-b256-b677d8ec7caa",
+        "https://osmt.wgu.edu/api/collections/7299e6a4-a679-4e2f-a604-b6ec696a0497"
+    ]
+}
+
+health_skills = {
+    "Healthcare Management": [
+        "https://osmt.wgu.edu/api/collections/29e22400-5ed7-4d64-9d15-b22748151edc",
+        "https://osmt.wgu.edu/api/collections/101dff66-19e2-4fec-a08b-e5339aeabc95"
+    ],
+    "Community and Social Service": [
+        "https://osmt.wgu.edu/api/collections/4beae755-f8dc-4599-b9b6-d4cd44b831a2",
+        "https://osmt.wgu.edu/api/collections/d3396ea9-1ad0-457e-a9c9-7a172aed5bd0",
+        "https://osmt.wgu.edu/api/collections/3bfed70f-ebb4-4c82-9bea-6b75e8d1a75f",
+        "https://osmt.wgu.edu/api/collections/131680ce-d0e3-495b-afe9-2fe6687261b0"
+    ],
+    "Healthcare Support": [
+        "https://osmt.wgu.edu/api/collections/8c7da71c-ba6a-45f9-8a16-f3626e978377",
+        "https://osmt.wgu.edu/api/collections/758bbdd3-f64a-4682-abdb-0f4da5a49112",
+        "https://osmt.wgu.edu/api/collections/950a0ba5-a70f-431d-a13a-ecefbb5d6bb7",
+        "https://osmt.wgu.edu/api/collections/b0040847-6e56-48a9-ba3e-563f9c6ab354",
+        "https://osmt.wgu.edu/api/collections/f356f15c-5972-4b8b-9a69-c0beb2ba524c"
+    ],
+    "Healthcare Practitioners": [
+        "https://osmt.wgu.edu/api/collections/aefea103-8c03-4c2c-8135-1e957dac5818",
+        "https://osmt.wgu.edu/api/collections/327f7354-0174-4ca7-b0a4-19022b3c6218",
+        "https://osmt.wgu.edu/api/collections/f2e295e7-8726-430e-9755-1ec743eb63e3",
+        "https://osmt.wgu.edu/api/collections/21530e26-5db0-417b-bb18-63ede1d734bd",
+        "https://osmt.wgu.edu/api/collections/14ed46a5-0c38-4fe8-ad2c-e2ba5a732f62",
+        "https://osmt.wgu.edu/api/collections/25f241e2-e3bc-4863-9dab-7a50faf4ad92",
+        "https://osmt.wgu.edu/api/collections/e5e3b41d-4d65-407a-9e1a-b36007464d8a",
+        "https://osmt.wgu.edu/api/collections/a9f1d114-6664-483e-a780-4a0cb926dbe5",
+        "https://osmt.wgu.edu/api/collections/91ef89c0-dea4-44f2-8f86-fa40a2dd092b",
+        "https://osmt.wgu.edu/api/collections/2b5ca8da-44fd-461b-8ea7-0f9c314e4084"
+    ],
+    "Healthcare Technology": [
+        "https://osmt.wgu.edu/api/collections/bf4b7cca-86cd-4a53-9428-03d4fe329ade"
+    ],
+    "Healthcare Standards": [
+        "https://osmt.wgu.edu/api/collections/42301ef2-0b16-4211-8d8e-842ee2cd4885"
+    ],
+    "Specialty": [
+        "https://osmt.wgu.edu/api/collections/d0ae6eb2-b356-4a9c-bdea-c18ad44b7a90"
+    ],
+    "Counseling": [
+        "https://osmt.wgu.edu/api/collections/91861083-382e-43cd-ab29-8aa525c3fa14",
+        "https://osmt.wgu.edu/api/collections/718ab258-969e-43df-ba57-ff6fc0211267"
+    ]
+}
+
+education_skills = {
+    "Specialty": [
+        "https://osmt.wgu.edu/api/collections/2fe8ca5f-a380-439a-b2a5-9ee5f28b920a",
+        "https://osmt.wgu.edu/api/collections/7826aee6-7147-4bc5-bdf3-0557fc321152",
+        "https://osmt.wgu.edu/api/collections/05e9bb2a-6d00-4798-9980-92a9c3b1ce79"
+    ],
+    "Educational Standards": [
+        "https://osmt.wgu.edu/api/collections/a98ec15a-2ab8-43e7-83cb-5f077fbc3599",
+        "https://osmt.wgu.edu/api/collections/407bb644-f284-4f4d-b4f1-7af77c1cd880",
+        "https://osmt.wgu.edu/api/collections/be4df083-3d11-47b9-9d83-b6d0ef5e95fa"
+    ],
+    "Educational Instruction": [
+        "https://osmt.wgu.edu/api/collections/b7b4ff91-91c6-46fc-a51b-8788373700b5",
+        "https://osmt.prod.wgu.edu/collections/6d119522-5ba8-4d8a-8b2a-625c5bedfb63",
+        "https://osmt.wgu.edu/api/collections/78d600af-03fc-4c2c-a024-510711ea47cc",
+        "https://osmt.wgu.edu/api/collections/c8a33205-757c-42d5-a189-8a8d031f2f71",
+        "https://osmt.wgu.edu/api/collections/a38a9f84-578c-4a01-83af-d4f62845ebbe",
+        "https://osmt.wgu.edu/api/collections/ab4328ba-1dd9-4f4f-8c8e-3f3aa0a65c09",
+        "https://osmt.wgu.edu/api/collections/d50add3a-7c9d-4232-8754-fb8f6487c4d4",
+        "https://osmt.wgu.edu/api/collections/6d51b268-1953-4952-bc5e-739b29892a43",
+        "https://osmt.wgu.edu/api/collections/61b8a8ac-42bc-4efa-b4bb-0f4593201fd6",
+        "https://osmt.wgu.edu/api/collections/b9cfb7c4-081c-4943-83e7-ec74e96b8c0b",
+        "https://osmt.wgu.edu/api/collections/8a12963d-446b-439b-a070-e9c85a17cb1c",
+        "https://osmt.wgu.edu/api/collections/3df911aa-9566-4345-90b0-dacfc63336d7",
+        "https://osmt.wgu.edu/api/collections/c27a9cfa-2958-4f9c-9b87-d21915606463",
+        "https://osmt.wgu.edu/api/collections/4043f96d-57fe-40da-94f5-23f90c9776f9",
+        "https://osmt.wgu.edu/api/collections/f67400c8-d00e-4bd0-bb32-2f6fe5776508"
+    ],
+    "Administration and Leadership": [
+        "https://osmt.wgu.edu/api/collections/86d75f52-e12b-423e-bc05-27561b33a767",
+        "https://osmt.wgu.edu/api/collections/18044bc8-f4f9-491a-830b-f804dfcde2b7",
+        "https://osmt.wgu.edu/api/collections/ca120f25-a61c-41c7-b4c5-2e0b3393fe95"
+    ],
+    "Design for Learning": [
+        "https://osmt.wgu.edu/api/collections/c53cb96a-46a7-4e87-bff8-31ad199c95eb",
+        "https://osmt.wgu.edu/api/collections/85c93bc0-e0c1-4b7d-8511-ce559e70f4cd"
+    ],
+    "Library": [
+        "https://osmt.wgu.edu/api/collections/3f52f212-44b0-46e6-9e19-8f2783908fab"
+    ]
+}
+
+information_technology_skills = {
+    "General IT": [
+        "https://osmt.wgu.edu/api/collections/6de62ad0-4a4c-4076-b3e1-65b991474b09",
+        "https://osmt.wgu.edu/api/collections/3797f212-b261-4d92-bc8b-7d604a5276f4",
+        "https://osmt.wgu.edu/api/collections/a2bbc89e-6312-4869-94ba-e7af80b42b6c",
+        "https://osmt.wgu.edu/api/collections/c0833f1c-809a-4003-9829-348332a49983",
+        "https://osmt.wgu.edu/api/collections/908a3821-5cbb-4c2e-8c6e-f340e745cc03",
+        "https://osmt.wgu.edu/api/collections/29cfbc42-8850-4207-9e4a-e07bd0f93296"
+    ],
+    "Cloud": [
+        "https://osmt.wgu.edu/api/collections/b48a9c23-08f2-4b27-aa39-1f649881f73a"
+    ],
+    "Information Systems": [
+        "https://osmt.wgu.edu/api/collections/37c8ee1c-0413-4bcf-9f4b-cbbea17adf2d"
+    ],
+    "Networking": [
+        "https://osmt.wgu.edu/api/collections/17bcc3a9-1b47-4f7d-8161-3cf1165490f0",
+        "https://osmt.wgu.edu/api/collections/5ee310e2-9d62-4da9-8202-d009becf6245",
+        "https://osmt.wgu.edu/api/collections/a413d731-0477-4982-b05a-3c93447efb89",
+        "https://osmt.wgu.edu/api/collections/e9801878-59c7-41b5-b195-9480236206bd",
+        "https://osmt.wgu.edu/api/collections/e3892a84-4463-4814-81df-d70e972b91c4"
+    ],
+    "Software Engineering": [
+        "https://osmt.wgu.edu/api/collections/ba52215b-5cae-4ce6-93de-a8684bb8bf56",
+        "https://osmt.wgu.edu/api/collections/168fae01-783d-422e-8b9f-93b9dc9102ea",
+        "https://osmt.wgu.edu/api/collections/09f17d00-1e68-4cab-b2f0-ab434433a694",
+        "https://osmt.wgu.edu/api/collections/734b8604-3c73-4503-b6b4-07b75e7e90cf",
+        "https://osmt.wgu.edu/api/collections/10bb727d-ed10-40a7-b3b5-c3e1e39b128e",
+        "https://osmt.wgu.edu/api/collections/aa9149fe-d86f-42af-9c3d-eadc6081fedf",
+        "https://osmt.wgu.edu/api/collections/14b7b9db-1105-45b2-9db3-ef0fcaee4aee",
+        "https://osmt.wgu.edu/api/collections/a5aa189e-ba42-405e-9ba3-83e177c39c91"
+    ],
+    "Security": [
+        "https://osmt.wgu.edu/api/collections/3cc1f145-a9c1-4d3c-8ab4-c08de1b08662",
+        "https://osmt.wgu.edu/api/collections/338e7f60-7649-4ebf-b8df-0b79fdc87ff0",
+        "https://osmt.wgu.edu/api/collections/aa013cc4-0423-4e07-9b1e-311e90e98900",
+        "https://osmt.wgu.edu/api/collections/8ec254ff-0729-47de-a05a-667ec748fd04"
+    ],
+    "Data Analytics": [
+        "https://osmt.wgu.edu/api/collections/e184627b-cf2c-4d07-8d72-294069ca15cd",
+        "https://osmt.wgu.edu/api/collections/508ae968-3460-42d6-b497-0243df300c57",
+        "https://osmt.wgu.edu/api/collections/1f90cce5-3905-4f08-b374-3a4f4e0a936d"
+    ],
+    "Data": [
+        "https://osmt.wgu.edu/api/collections/cc9d0f02-d8a3-459a-9339-943144ea8a0a"
+    ],
+    "Database": [
+        "https://osmt.wgu.edu/api/collections/9def40ce-3603-4fd2-9400-e797c3ed7994"
+    ],
+    "Technology/Technician": [
+        "https://osmt.wgu.edu/api/collections/0292f1c7-f159-4dba-bc02-af8c06a6de7c"
+    ],
+    "Forensics": [
+        "https://osmt.wgu.edu/api/collections/6642cf6a-6960-4753-a1a1-044a7f012275"
+    ]
+}
+
+engineering_skills = {
+    "Environmental": [
+        "https://osmt.wgu.edu/api/collections/788e520c-a60c-44fb-b378-f213b37c97ee"
+    ],
+    "Industrial": [
+        "https://osmt.wgu.edu/api/collections/79399575-3936-47f2-8848-b95a2d39dfd5"
+    ]
+}
---
+++ src/skills_fetch.py
@@ -0,0 +1,115 @@
+import requests
+from bs4 import BeautifulSoup
+import time
+import pandas as pd
+from .dataset import (
+    business_skills,
+    specialty_skills,
+    law_skills,
+    health_skills,
+    education_skills,
+    information_technology_skills,
+    engineering_skills,
+)
+
+def scrape_skills_from_page(url):
+
+    response = requests.get(url)
+    
+    if response.status_code == 200:
+        soup = BeautifulSoup(response.text, 'html.parser')
+        
+        table = soup.find('table', {'class': 'l-table'})
+        
+        if table:
+            skills_data = []
+            
+            rows = table.find('tbody').find_all('tr')
+            
+            for row in rows:
+                columns = row.find_all('td')
+                
+                if len(columns) > 1:
+                    # extract the relevant columns (the first column is "Categories" and the second is "Rich Skill Descriptor")
+                    category = columns[0].get_text(strip=True)
+                    skill_descriptor = columns[1].get_text(strip=True)
+                    
+                    skills_data.append({
+                        'category': category,
+                        'rich_skill_descriptor': skill_descriptor
+                    })
+                    
+            return skills_data
+        else:
+            print(f"Table not found on page: {url}")
+            return None
+    else:
+        print(f"Failed to fetch the page: {url}, Status Code: {response.status_code}")
+        return None
+
+def get_total_pages(soup):
+    """Extract total number of pages from pagination element."""
+    pagination = soup.find('div', {'class': 'm-pagination'})
+    if pagination:
+        page_info = pagination.find('p', {'class': 'm-pagination-x-total'}).get_text(strip=True)
+        total_pages = int(page_info.split(" of ")[1])  # extract total number of pages
+        return total_pages
+    return 1  # default to 1 if pagination not found
+
+def scrape_all_pages(base_url):
+    current_page = 1
+    all_skills = []
+    
+    while True:
+        page_url = f"{base_url}?page={current_page}"
+        print(f"Scraping page {current_page}: {page_url}")
+        
+        response = requests.get(page_url)
+        if response.status_code != 200:
+            print(f"Failed to fetch page {current_page}, stopping.")
+            break
+        
+        soup = BeautifulSoup(response.text, 'html.parser')
+        
+        if current_page == 1:
+            total_pages = get_total_pages(soup)
+            print(f"Total pages found: {total_pages}")
+        
+        skills = scrape_skills_from_page(page_url)
+        if skills:
+            all_skills.extend(skills)
+        else:
+            print(f"No skills found on page {current_page}.")
+        
+        if current_page >= total_pages:
+            break
+        
+        current_page += 1
+        
+        time.sleep(5)
+
+    return all_skills
+
+
+def main():
+
+    skill_types = [
+        business_skills,
+        specialty_skills,
+        law_skills,
+        health_skills,
+        education_skills,
+        information_technology_skills,
+        engineering_skills
+    ]
+
+    urls_list = [url for skill_type in skill_types for sublist in skill_type.values() for url in sublist]
+
+    for url in urls_list:
+        skills_data = scrape_all_pages(url)
+
+        skills_df = pd.DataFrame.from_dict(skills_data, orient='index').transpose()
+
+        print(skills_df)
+
+
